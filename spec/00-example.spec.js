84ccae5b4be4f6588aed1fcf1e2c48a3194ef659
Filippo Mangione <filippomangione@outlook.com>
2026-02-16 10:10:08 +0100

    chore: loop example

--- spec/00-example.spec.js
+++ spec/00-example.spec.js
@@ -1,4 +1,6 @@
-const { a, b } = require('../src/00-example.js');
+
+const { a, b, c: sumToN } = require('../src/00-example.js');
+
 
 describe('example function', () => {
   test("returns the string 'Hi, Ed'", () => {
@@ -9,3 +11,18 @@ describe('example function', () => {
     expect(b(1)).toBe(2);
   });
 });
+
+describe('sumToN', () => {
+  it('sums from 1 to 3', () => {
+    expect(sumToN(3)).toBe(6)
+  })
+  it('sums from 1 to 5', () => {
+    expect(sumToN(5)).toBe(15)
+  })
+  it('sums from 1 to 1', () => {
+    expect(sumToN(1)).toBe(1)
+  })
+  it('sums from 1 to 0 (edge case)', () => {
+    expect(sumToN(0)).toBe(0)
+  })
+})
--- src/00-example.js
+++ src/00-example.js
@@ -1,3 +1,16 @@
+
+// Example: Defune a function that returns the sum of all numbers from 1 to n using a for loop
+// Input | Output
+// 3     | 6 (1+2+3)
+// 5     | 15 (1+2+3+4+5)
+
+function sumToN(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
 // Completed function examples
 
 // Define a function that returns the string "Hi, Ed!"
@@ -20,5 +33,6 @@ function incrementNumber(num) {
 // change the exported value to be the name of the function you defined
 module.exports = {
   a: sayHi,
-  b: incrementNumber
+  b: incrementNumber,
+  c: sumToN
 }

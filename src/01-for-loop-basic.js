8e97f17bdacc7d9da2ac4c4f01517e9fe12a22ad
willymariino <wg.mariino@gmail.com>
2026-02-25 00:17:39 +0100

    set 1 for loops basics completato

--- src/01-for-loop-basic.js
+++ src/01-for-loop-basic.js
@@ -3,7 +3,10 @@ const numsFiveToTen = []
 const evenNums = []
 const countdown = []
 
-console.log("file caricato")
+//comando per attivare il refresh:
+// nodemon --exec "cls && node src/01-for-loop-basic.js"
+
+console.log("Esercizio 1")
 // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
 
 for (let i = 0; i <= 3; i++) {
@@ -13,9 +16,35 @@ console.log(numsZeroToThree)
 
 // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
 
+console.log("esercizio 2")
+
+for (let i = 5; i <= 10; i++) {
+  numsFiveToTen.push(i)
+}
+
+console.log(numsFiveToTen)
+
 // TODO: 3. Write a for loop that adds all the even numbers between 0 and 6 (0, 2, 4, 6) to evenNums
 
+console.log("esercizio 3")
+
+for (let i = 0; i <= 6; i++) {
+  if ([i] % 2 === 0) {
+    evenNums.push(i)
+  }
+}
+
+console.log(evenNums)
+
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
+
+console.log("esercizio 4")
+
+for (let i = 3; i >= 0; i--) {
+  countdown.push(i)
+}
+
+console.log(countdown)
 
 // do not change below this line
 

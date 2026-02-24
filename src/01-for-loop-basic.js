08c3004aaf628b8abf16a382ca36cc7882095470
willymariino <wg.mariino@gmail.com>
2026-02-24 20:00:48 +0100

    esercizio 1 file 01

--- src/01-for-loop-basic.js
+++ src/01-for-loop-basic.js
@@ -3,7 +3,13 @@ const numsFiveToTen = []
 const evenNums = []
 const countdown = []
 
+console.log("file caricato")
 // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array
+
+for (let i = 0; i <= 3; i++) {
+  numsZeroToThree.push(i)
+}
+console.log(numsZeroToThree)
 
 // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
 
@@ -12,6 +18,9 @@ const countdown = []
 // TODO: 4. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
 
 // do not change below this line
+
+
+
 module.exports = {
   a: numsZeroToThree,
   b: numsFiveToTen,
